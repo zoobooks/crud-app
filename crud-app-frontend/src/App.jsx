b902c14618b3c5e83301a6373a1424424c0a2a69
zoobooks <zblaise23@gmail.com>
2022-09-11 14:54:59 -0500

    got react installed using CRA (against my better judgement) and material UI is installed as well

---
+++ crud-app-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import './PersonListing.jsx';
+import PersonListing from './PersonListing.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PersonListing/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ crud-app-frontend/src/PersonListing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PersonListing = () => {
+    
+return(
+
+    <div>
+        Person Listing
+    </div>
+)    
+
+
+}
+
+export default PersonListing;
